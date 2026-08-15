2a34dafc17e1b48b4a30ffbbbf37411a2cd675f9
Anamikasingh07 <annesingh077@gmail.com>
2026-08-15 16:57:45 +0530

    prepare backend for Render

--- Server/server.js
+++ Server/server.js
@@ -637,12 +637,8 @@ function leaveRoom(socket) {
 // START SERVER
 // ==========================================
 
-server.listen(
-    5000,
-    () => {
+const PORT = process.env.PORT || 5000;
 
-        console.log(
-            "Server running on http://localhost:5000"
-        );
-    }
-);
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
