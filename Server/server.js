9de3df8bf338579b1686b9dbdb62aeb92ebdcef5
Anamikasingh07 <annesingh077@gmail.com>
2026-08-15 17:26:05 +0530

    Fix production CORS

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://meetflow-m2t5.onrender.com",
         methods: ["GET", "POST"],
     })
 );
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://meetflow-m2t5.onrender.com",
         methods: ["GET", "POST"],
     },
 });
